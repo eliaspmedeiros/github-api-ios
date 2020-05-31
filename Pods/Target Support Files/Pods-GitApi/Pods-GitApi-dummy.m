7ddb466da64d8fd3371b96c1e38f4067aa4ca315
Elias Medeiros <elias.medeiros@cwi.com.br>
2020-05-30 21:25:23 -0300

    chore: remove pods from versioning

--- Pods/Target Support Files/Pods-GitApi/Pods-GitApi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GitApi : NSObject
-@end
-@implementation PodsDummy_Pods_GitApi
-@end
--- Pods/Target Support Files/Pods-GitApiTests/Pods-GitApiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GitApiTests : NSObject
-@end
-@implementation PodsDummy_Pods_GitApiTests
-@end
